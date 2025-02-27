160e0ad77349b2c7f5eda852a665e3a4eef8dba0
Leo-Slv <leonardo1692004@gmail.com>
2025-02-27 19:10:39 -0300

    First commit project SHOP-API

---
+++ src/test/java/com/example/SHOP_API/ShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SHOP_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
